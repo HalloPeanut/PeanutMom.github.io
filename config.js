56454081fa2cd09de2d9c879a61f93bb4ea8eb33
HalloPeanut <106302012+HalloPeanut@users.noreply.github.com>
2022-10-19 23:37:24 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'MẸ';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng CHO MẸ NÈ';
+const contentCard = 'Chúc mẹ ngày 20/10 của con luôn tràn ngập niềm vui và nụ cười, say này mỗi ngày đều vui vẻ, ở bên cạnh con mãi mãi <3 <3';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'mom.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
